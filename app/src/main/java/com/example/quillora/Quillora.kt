725c84bc923358157ee1c2712644235ab612a6ac
emran <ahmed777emran@gmail.com>
2026-03-10 16:20:56 +0600

    feat(di): setup dependency injection

---
+++ app/src/main/java/com/example/quillora/Quillora.kt
@@ -0,0 +1,11 @@
+package com.example.quillora
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @author EMRAN AHMED
+ */
+
+@HiltAndroidApp
+class Quillora: Application()
