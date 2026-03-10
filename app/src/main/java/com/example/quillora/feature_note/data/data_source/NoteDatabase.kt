1c18c291a051890ef9b9c768977e17b106de273c
emran <ahmed777emran@gmail.com>
2026-03-10 16:18:06 +0600

    feat(data): create model, dao, and database

---
+++ app/src/main/java/com/example/quillora/feature_note/data/data_source/NoteDatabase.kt
@@ -0,0 +1,22 @@
+package com.example.quillora.feature_note.data.data_source
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.quillora.feature_note.domain.model.Note
+
+/**
+ * @author EMRAN AHMED
+ */
+
+@Database(
+    entities = [Note::class],
+    version = 1
+)
+abstract class NoteDatabase: RoomDatabase() {
+
+    abstract val noteDao: NoteDao
+
+    companion object{
+        const val DATABASE_NAME = "notes_db"
+    }
+}
