1cea7d413e4b1209a1f3bf079a68e744f010aa1a
emran <ahmed777emran@gmail.com>
2026-03-12 16:19:45 +0600

    feat(notes): design note screen

---
+++ app/src/main/java/com/example/quillora/feature_note/presentation/notes/NoteScreen.kt
@@ -0,0 +1,157 @@
+package com.example.quillora.feature_note.presentation.notes
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Sort
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.quillora.feature_note.presentation.notes.components.NoteItem
+import com.example.quillora.feature_note.presentation.notes.components.OrderSection
+import com.example.quillora.feature_note.presentation.notes.viewmodel.NotesViewModel
+import kotlinx.coroutines.launch
+
+/**
+ * @author EMRAN AHMED
+ */
+
+@Composable
+fun NoteScreen(
+    navController: NavController,
+    viewModel: NotesViewModel = hiltViewModel()
+){
+    val state = viewModel.state.value
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                containerColor = FloatingActionButtonDefaults.containerColor
+            ) {
+                Icon(
+                    Icons.Default.Add,
+                    contentDescription = "Add Note"
+                )
+            }
+        },
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }
+    ) {innerPadding ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Your note",
+                    style = MaterialTheme.typography.titleLarge
+                )
+                IconButton(
+                    onClick = {
+                        viewModel.onEvent(NoteEvent.ToggleOrderSection)
+                    },
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.Sort,
+                        contentDescription = "Sort"
+                    )
+                }
+            }
+        }
+
+        AnimatedVisibility(
+            visible = state.isOrderSectionVisible,
+            enter = fadeIn() + expandVertically(),
+            exit = fadeOut() + shrinkVertically()
+        ) {
+
+            OrderSection(
+                modifier = Modifier,
+                noteOrder = state.noteOrder,
+                onChangeOrder = {
+                    viewModel.onEvent(NoteEvent.Order(it))
+                }
+            )
+        }
+        Spacer(Modifier.height(16.dp))
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(state.notes){ note->
+                NoteItem(
+                    note = note,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clickable{
+
+                        }
+                    ,
+                    onDeleteClick = {
+                        viewModel.onEvent(NoteEvent.DeleteNote(note))
+
+                        scope.launch {
+                            val result = snackbarHostState.showSnackbar(
+                                message = "Note deleted",
+                                actionLabel = "Undo"
+                            )
+                            if(result == SnackbarResult.ActionPerformed){
+                                viewModel.onEvent(NoteEvent.RestoreNote)
+                            }
+                        }
+                    }
+                )
+                Spacer(Modifier.height(16.dp))
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NoteScreenPreview(){
+    NoteScreen(navController = NavController(LocalContext.current))
+}
