ab122bc6c82f412e9f31978f2b3e2530aea8cc02
emran <ahmed777emran@gmail.com>
2026-03-29 15:14:35 +0600

    feat: add files

---
+++ app/src/main/java/com/example/quillora/feature_note/presentation/add_edit_note/NoteTextFieldState.kt
@@ -0,0 +1,10 @@
+package com.example.quillora.feature_note.presentation.add_edit_note
+
+/**
+ * @author EMRAN AHMED
+ */
+data class NoteTextFieldState(
+    val text: String = "",
+    val hint: String = "",
+    val isHintVisible: Boolean = true
+)
--- app/src/main/java/com/example/quillora/feature_note/presentation/add_edit_note/viewmodel/AddEditNoteViewModel.kt
+++ app/src/main/java/com/example/quillora/feature_note/presentation/add_edit_note/viewmodel/AddEditNoteViewModel.kt
@@ -1,9 +1,14 @@
 package com.example.quillora.feature_note.presentation.add_edit_note.viewmodel
 
 import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.toArgb
 import androidx.lifecycle.ViewModel
+import com.example.quillora.feature_note.domain.model.Note
 import com.example.quillora.feature_note.domain.use_case.NoteUseCases
+import com.example.quillora.feature_note.presentation.add_edit_note.NoteTextFieldState
+import kotlinx.coroutines.flow.MutableSharedFlow
 import javax.inject.Inject
 
 /**
@@ -13,8 +18,14 @@ class AddEditNoteViewModel @Inject constructor(
     private val noteUseCases: NoteUseCases
 ) : ViewModel() {
 
-    private val _noteTitle = mutableStateOf("")
-    val noteTitle: State<String> = _noteTitle
+    private val _noteTitle = mutableStateOf(NoteTextFieldState())
+    val noteTitle: State<NoteTextFieldState> = _noteTitle
 
-    
+    private val _noteContent = mutableStateOf(NoteTextFieldState())
+    val noteContent: State<NoteTextFieldState> = _noteContent
+
+    private val _noteColor = mutableIntStateOf(Note.noteColors.random().toArgb())
+    val noteColor: State<Int> = _noteColor
+
+    private val _eventFlow = MutableSharedFlow<>()
 }
